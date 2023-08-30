ebd0452b9bc14766d314c091e8ee2d7ed6e1b211
André De Carli <a.decarli89@gmail.com>
2023-08-30 18:48:18 -0300

    dependencies and initial app

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', async () => {
+  return { message: 'Hello World' }
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running')
+  })
